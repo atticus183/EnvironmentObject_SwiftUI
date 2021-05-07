4f65b385b23d60e1a80651b8ea34b8f6081e4e1f
Josh R <seventytwoapp@gmail.com>
2021-05-06 21:04:45 -0500

    Initial Commit

---
+++ EnvironmentObject_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EnvironmentObject_SwiftUI
+//
+//  Created by Josh R on 5/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EnvironmentObject_SwiftUI/EnvironmentObject_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObject_SwiftUIApp.swift
+//  EnvironmentObject_SwiftUI
+//
+//  Created by Josh R on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObject_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
